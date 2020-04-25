22fdd8170fad9117bc1e608430779ec707afec9c
Ilhan Klisura <ilhank.official@gmail.com>
2020-04-25 23:40:23 +0200

    CoronaVirus Tracking App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+//import Cards from './components/Cards';
+//import Charts from './components/Charts';
+//import CountryPicker from './components/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+import coronaImage from './images/image.png';
 class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+    
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData, country: country });
+    }
+
     render() {
+        const { data, country } = this.state;
+
         return(
-            <h1><div>Coronavirus Tracker</div></h1>
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19" />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+            </div>
         )
     }
 }
